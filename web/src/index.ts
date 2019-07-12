2dca5f189763015203e3f2d3e435469d96b25ca5
Ante <tolusic9@gmail.com>
2019-07-12 20:42:24 +0200

    Web Framework/Add evenets as a separate class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,5 @@
-import axios from 'axios'
+import {User} from './models/User'
 
-axios.post('http://localhost:3000/users', {
-  name: 'myname',
-  age: 23,
-})
+const user = new User({name: 'new record', age: 1})
+
+user.save()
